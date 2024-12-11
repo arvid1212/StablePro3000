e5933dd81595e7da30d272503753ecf0993626fb
arvid1212 <145985929+arvid1212@users.noreply.github.com>
2024-12-11 17:44:20 +0100

    reset

---
+++ StablePro/app/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Welcome to StablePro 
+
+        MY horses
+
+        My Stables
+      </Text>
+    </View>
+  );
+}
